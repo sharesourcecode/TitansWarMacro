1ad70eea46698a51354bf9988e42d4ed18267128
Ueliton <49400447+AlvesUeliton@users.noreply.github.com>
2019-10-11 20:19:02 -0300

    Update macros.sh

--- macros.sh
+++ macros.sh
@@ -168,10 +168,9 @@ function _arena () {
 	ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep "${PAC[0]}" | head -n1 | cut -d\' -f2) #/arena/attack/1/1234567*/
 	EXIT=$(echo $SRC | sed 's/href=/\n/g' | grep "${PAC[1]}" | head -n1 | cut -d\' -f2) #/lab/wizard/potion/1234567*/?ref=/arena/
 	while [[ -z $EXIT ]]; do
-		echo "$ACCESS"
 		SRC=$(w3m -cookie -debug -dump_source -o accept_encoding=='*;q=0' "$URL$ACCESS" -o user_agent="$(shuf -n1 .ua)")
 		ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep "${PAC[0]}" | head -n1 | cut -d\' -f2) #/arena/attack/1/1234567*/
-		sleep 2 && echo "$ACCESS"
+		echo "$ACCESS"
 		EXIT=$(echo $SRC | sed 's/href=/\n/g' | grep "${PAC[1]}" | head -n1 | cut -d\' -f2) #/lab/wizard/potion/1234567*/?ref=/arena/
 	done
 }
