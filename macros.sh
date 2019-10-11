1fe210d1f049afe229715e5f4c607069a333d5cd
Ueliton <49400447+AlvesUeliton@users.noreply.github.com>
2019-10-10 22:24:53 -0300

    Update macros.sh

--- macros.sh
+++ macros.sh
@@ -162,7 +162,7 @@ function _time () {
 }
 _time
 # //clan id - - - - - - - - - - - - - - - - - - - - - - - - -
-SRC=$(w3m -debug -dump_source -o accept_encoding=='*;q=0' $URL/clan -o user_agent="$(shuf -n1 .ua)")
+SRC=$(w3m -debug -dump_source -o accept_encoding=='*;q=0' "$URL/clan" -o user_agent="$(shuf -n1 .ua)")
 CLD=$(echo $SRC | sed "s/\/clan\//\\n/g" | grep 'built\/' | cut -d\/ -f1)
 # //arena - - - - - - - - - - - - - - - - - - - - - - - - - - -
 function _arena () {
