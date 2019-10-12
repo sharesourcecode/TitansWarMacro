0f301f6bf9ddf773b433ba852e9153a35a43af0c
Ueliton <49400447+AlvesUeliton@users.noreply.github.com>
2019-10-11 21:22:34 -0300

    Update macros.sh

--- macros.sh
+++ macros.sh
@@ -166,13 +166,13 @@ CLD=$(echo $SRC | sed "s/\/clan\//\\n/g" | grep 'built\/' | cut -d\/ -f1)
 function _arena () {
 	PAC=( 'arena/attack' 'lab/wizard' ) #Page:Action:Condition
 	SRC=$(w3m -cookie -debug -dump_source -o accept_encoding=='*;q=0' "$URL/${PAC[0]:0:5}" -o user_agent="$(shuf -n1 .ua)")
-	ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep "${PAC[0]}" | head -n1 | cut -d\' -f2) #/arena/attack/1/1234567*/
-	EXIT=$(echo $SRC | sed 's/href=/\n/g' | grep "${PAC[1]}" | head -n1 | cut -d\' -f2) #/lab/wizard/potion/1234567*/?ref=/arena/
+	ACCESS="$(echo $SRC | sed 's/href=/\n/g' | grep "${PAC[0]}" | head -n1 | cut -d\' -f2)" #/arena/attack/1/1234567*/
+	EXIT="$(echo $SRC | sed 's/href=/\n/g' | grep "${PAC[1]}" | head -n1 | cut -d\' -f2)" #/lab/wizard/potion/1234567*/?ref=/arena/
 	while [[ -z $EXIT ]]; do
 		SRC=$(w3m -cookie -debug -dump_source -o accept_encoding=='*;q=0' "$URL$ACCESS" -o user_agent="$(shuf -n1 .ua)")
-		ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep "${PAC[0]}" | head -n1 | cut -d\' -f2) #/arena/attack/1/1234567*/
+		ACCESS="$(echo $SRC | sed 's/href=/\n/g' | grep "${PAC[0]}" | head -n1 | cut -d\' -f2)" #/arena/attack/1/1234567*/
 		echo "$ACCESS"
-		EXIT=$(echo $SRC | sed 's/href=/\n/g' | grep "${PAC[1]}" | head -n1 | cut -d\' -f2) #/lab/wizard/potion/1234567*/?ref=/arena/
+		EXIT="$(echo $SRC | sed 's/href=/\n/g' | grep "${PAC[1]}" | head -n1 | cut -d\' -f2)" #/lab/wizard/potion/1234567*/?ref=/arena/
 	done
 }
 # //campaign- - - - - - - - - - - - - - - - - - - - - - - - -
