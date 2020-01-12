cba929cf052cb7cfd7bc05439783d275275a9fd9
Ton Guitar <ueliton17071987@gmail.com>
2020-01-12 10:50:31 -0300

    Add files via upload

--- campaign.sh
+++ campaign.sh
@@ -1,12 +1,12 @@
 # //campaign- - - - - - - - - - - - - - - - - - - - - - - - -
 _campaign () {
 	SRC=$(w3m -cookie -debug -dump_source -o accept_encoding=='*;q=0' "$URL/campaign" -o user_agent="$(shuf -n1 .ua)")
-#	SRC=$(lynx -cfg=cfg1 -source "$URL/campaign" -useragent="$(shuf -n1 .ua)")
+#	SRC=$(lynx -cfg=~/twm/cfg1 -source "$URL/campaign" -useragent="$(shuf -n1 .ua)")
 	ENTER=$(echo $SRC | sed "s/href='/\n/g" | grep "/campaign/" | head -n1 | cut -d\' -f1 | cut -d\/ -f3)
 	ACCESS=$(echo $SRC | sed "s/href='/\n/g" | grep "/campaign/" | head -n1 | cut -d\' -f1)
 	while [[ -n $ENTER && -n $ACCESS ]]; do
 		SRC=$(w3m -cookie -debug -dump_source -o accept_encoding=='*;q=0' $URL$ACCESS -o user_agent="$(shuf -n1 .ua)")
-#		SRC=$(lynx -cfg=cfg1 -source $URL$ACCESS -useragent="$(shuf -n1 .ua)")
+#		SRC=$(lynx -cfg=~/twm/cfg1 -source $URL$ACCESS -useragent="$(shuf -n1 .ua)")
 		ENTER=$(echo $SRC | sed "s/href='/\n/g" | grep "/campaign/" | head -n1 | cut -d\' -f1 | cut -d\/ -f3)
 		ACCESS=$(echo $SRC | sed "s/href='/\n/g" | grep "/campaign/" | head -n1 | cut -d\' -f1)
 		echo "$ACCESS"
