b0b03aa8e75d40fb18f6fcec296a84b07947e8d2
Ton Guitar <ueliton17071987@gmail.com>
2020-01-12 10:43:50 -0300

    Add files via upload

--- arena.sh
+++ arena.sh
@@ -2,12 +2,12 @@
 function _arena () {
 	PAC=( 'arena/attack' 'lab/wizard' ) #Page:Action:Condition
 	SRC=$(w3m -cookie -debug -dump_source -o accept_encoding=='*;q=0' "$URL/${PAC[0]:0:5}" -o user_agent="$(shuf -n1 .ua)")
-#	SRC=$(lynx -cfg=cfg1 -source "$URL/${PAC[0]:0:5}" -useragent="$(shuf -n1 .ua)")
+#	SRC=$(lynx -cfg=~/twm/cfg1 -source "$URL/${PAC[0]:0:5}" -useragent="$(shuf -n1 .ua)")
 	ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep "${PAC[0]}" | head -n1 | cut -d\' -f2) #/arena/attack/1/1234567*/
 	EXIT=$(echo $SRC | sed 's/href=/\n/g' | grep "${PAC[1]}" | head -n1 | cut -d\' -f2) #/lab/wizard/potion/1234567*/?ref=/arena/
 	while [[ -z $EXIT && -n $ACCESS ]]; do
 		SRC=$(w3m -cookie -debug -dump_source -o accept_encoding=='*;q=0' "$URL$ACCESS" -o user_agent="$(shuf -n1 .ua)")
-#		SRC=$(lynx -cfg=-cfg1 -source "$URL$ACCESS" -useragent="$(shuf -n1 .ua)")
+#		SRC=$(lynx -cfg=~/twm/cfg1 -source "$URL$ACCESS" -useragent="$(shuf -n1 .ua)")
 		ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep "${PAC[0]}" | head -n1 | cut -d\' -f2) #/arena/attack/1/1234567*/
 		echo "$ACCESS"
 		EXIT=$(echo $SRC | sed 's/href=/\n/g' | grep "${PAC[1]}" | head -n1 | cut -d\' -f2) #/lab/wizard/potion/1234567*/?ref=/arena/
