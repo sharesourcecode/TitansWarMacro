16fe7ab674e9c42e01fab659a5304be4804fb10c
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2020-01-16 16:39:41 -0300

    Add files via upload

--- clandungeon.sh
+++ clandungeon.sh
@@ -1,14 +1,15 @@
 # //clandungeon - - - - - - - - - - - - - - - - - - - - - - -
 function _clandungeon () {
+	_clanid
 	if [[ -n $CLD ]]; then
 		echo "Checking Clan Dungeon..."
 		SRC=$(w3m -debug -dump_source -o accept_encoding=='*;q=0' "$URL/clandungeon/?close" -o user_agent="$(shuf -n1 .ua)")
-		ACCESS=$(echo $SRC | sed "s/clandungeon/\\n/g" | grep "attack" | cut -d\/ -f3 | shuf -n1)
-		EXIST=$(echo $SRC | sed "s/clandungeon/\\n/g" | grep "attack" | cut -d\/ -f2 | shuf -n1)
-		while [[ $EXIST == attack ]]; do
-			SRC=$(w3m -debug -dump_source -o accept_encoding=='*;q=0' "$URL/clandungeon/attack/$ACCESS" -o user_agent="$(shuf -n1 .ua)")
-			ACCESS=$(echo $SRC | sed "s/clandungeon/\\n/g" | grep "attack" | cut -d\/ -f3 | shuf -n1)
-			EXIST=$(echo $SRC | sed "s/clandungeon/\\n/g" | grep "attack" | cut -d\/ -f2 | shuf -n1)
+		ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep '/clandungeon/attack/' | head -n1 | cut -d\' -f2)
+		EXIST=$(echo $SRC | sed 's/href=/\n/g' | grep -o 'clandungeon/attack')
+		until [[ -z $EXIST ]]; do
+			SRC=$(w3m -debug -dump_source -o accept_encoding=='*;q=0' "$URL$ACCESS" -o user_agent="$(shuf -n1 .ua)")
+			ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep '/clandungeon/attack/' | head -n1 | cut -d\' -f2)
+			EXIST=$(echo $SRC | sed 's/href=/\n/g' | grep -o 'clandungeon/attack')
 			echo " ⚔ $ACCESS"
 		done
 		echo -e "Clan Dungeon (✔)\n"
