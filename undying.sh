4a97990fcc9bbe9215e50bee1b59d3b733465996
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2020-01-26 17:33:20 -0300

    Update undying.sh

--- undying.sh
+++ undying.sh
@@ -2,40 +2,31 @@ _undying () {
 # /enterGame
 	echo "Undying"
 	w3m -debug -o accept_encoding=='*;q=0' $URL/undying/enterGame -o user_agent="$(shuf -n1 .ua)" | head -n5
-	sleep 2
 #
 	echo " 👣 Entering..."
 	SRC=$(w3m -debug -dump_source -o accept_encoding=='*;q=0' $URL/undying -o user_agent="$(shuf -n1 .ua)")
-# /wait
-	echo " 😴 Waiting..."
 	ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep '/undying/' | head -n1 | cut -d\' -f2)
-	EXIT=$(echo $SRC | grep -o 'undying/mana')
+	_access
+# /wait	
+	echo " 😴 Waiting..."
 	START=`date +%M`
-	sleep 2
-#
-	while [[ -z $EXIT ]] ; do
-		END=$(expr `date +%M` \- $START)
-		[[ $END -gt 15 ]] && break
-		SRC=$(w3m -debug -dump_source -o accept_encoding=='*;q=0' "$URL$ACCESS" -o user_agent="$(shuf -n1 .ua)")
-		ACCESS=$(echo $SRC | sed "s/href=/\n/g" | grep '/undying/' | cut -d\' -f2 | head -n1)
-		EXIT=$(echo $SRC | grep -o 'undying/mana/')
-		echo -e " 💤 	..."
-	done
-#
-	sleep 1
+	until [[ -n $MANA ]] ; do
+	END=$(expr `date +%M` \- $START)
+	[[ $END -gt 3 ]] && break
+	echo -e " 💤 	..."
 	SRC=$(w3m -debug -dump_source -o accept_encoding=='*;q=0' "$URL$ACCESS" -o user_agent="$(shuf -n1 .ua)")
-#	ACCESS=$(echo $SRC | sed 's/href=/\n/g'  | grep 'undying/mana/' | head -n1 | cut -d\' -f2)
-#	SRC=$(w3m -debug -dump_source -o accept_encoding=='*;q=0' "$URL$ACCESS" -o user_agent="$(shuf -n1 .ua)")
-	ACCESS=$(echo $SRC | sed 's/href=/\n/g'  | grep 'undying/hit/' | head -n1 | cut -d\' -f2)
-	EXIT=$(echo $SRC | grep -o 'hit/out_gate')
+	ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep '/undying/' | head -n1 | cut -d\' -f2)
+	_access
+	done
+	SRC=$(w3m -debug -dump_source -o accept_encoding=='*;q=0' "$URL$ACCESS" -o user_agent="$(shuf -n1 .ua)")
+	_access
 	START=`date +%M`
-	while [[ -n $EXIT ]] ; do
+	while [[ -n $OUTGATE ]] ; do
 		END=$(expr `date +%M` \- $START)
 		[[ $END -gt 5 ]] && break
 		echo -e " 🎲 hiting..."
-		SRC=$(w3m -debug -dump_source -o accept_encoding=='*;q=0' "$URL$ACCESS" -o user_agent="$(shuf -n1 .ua)")
-		ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep 'undying/hit/' | head -n1 | cut -d\' -f2)
-		EXIT=$(echo $SRC | grep -o 'hit/out_gate')
+		SRC=$(w3m -debug -dump_source -o accept_encoding=='*;q=0' "$URL$HIT" -o user_agent="$(shuf -n1 .ua)")
+		_access
 		sleep 5
 	done
 }
