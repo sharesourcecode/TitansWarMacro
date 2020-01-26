edc54a18c4828708ba8b94aeddbb3538cb01d8fa
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2020-01-26 19:32:00 -0300

    Update clanid.sh

--- clanid.sh
+++ clanid.sh
@@ -1,4 +1,4 @@
-# //clan id - - - - - - - - - - - - - - - - - - - - - - - - -
+# /clan id
 _clanid () {
 	SRC=$(w3m -cookie -debug -dump_source -o accept_encoding=='*;q=0' "$URL/clan" -o user_agent="$(shuf -n1 .ua)")
 #	SRC=$(lynx -cfg=~/twm/cfg1 -source "$URL/clan" -useragent="$(shuf -n1 .ua)")
