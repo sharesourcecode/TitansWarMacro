defa401900a54db4ec886101e5c1fa23a2f02ef7
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2020-01-26 19:03:36 -0300

    Update play.sh

--- play.sh
+++ play.sh
@@ -9,12 +9,12 @@ _play () {
 #		_torstop
 	}
 _crono
-# //game time
+# /game time
 	if [[ $HOUR -lt 8 || $HOUR -eq 23 ]] ; then
 		_all
 		_sleep
 		_crono
-# //Valley of the Immortals 10:00:00 - 16:00:00 - 22:00:00
+# /Valley of the Immortals 10:00:00 - 16:00:00 - 22:00:00
 	elif [[ $HOUR -eq 9 && $MIN -ge 45 || $HOUR -eq 15 && $MIN -ge 45 || $HOUR -eq 21 && $MIN -ge 45 ]] ; then
 		START=`date +%M`
 		while [[ $MIN -ge 45 && $MIN -le 59 ]] ; do
@@ -28,12 +28,12 @@ _crono
 			fi
 		done
 		_crono
-# //Battle of banners 10:15:00 - 16:15:00
+# /Battle of banners 10:15:00 - 16:15:00
 #	elif [[ $HOUR -eq 10 && $MIN -gt 10 && $MIN -lt 16 || $HOUR -eq 16 && $MIN -gt 10 && $MIN -lt 16 ]] ; then
 #		ts=300
 #		SRC=$(w3m -cookie -debug -dump_source -o accept_encoding=='*;q=0' $URLhttp/flagfight/enterFight -o user_agent="$(shuf -n1 .ua)")
 #		_crono
-# //Clan coliseum 10:30:00 - 15:00:00
+# /Clan coliseum 10:30:00 - 15:00:00
 	elif [[ $HOUR -eq 10 && $MIN -ge 15 && $MIN -le 30 || $HOUR -eq 14 && $MIN -ge 45 ]] ; then
 		START=`date +%M`
 		while [[ $MIN -ge 15 && $MIN -le 30 || $MIN -ge 45 && $MIN -le 59 ]] ; do
@@ -48,18 +48,18 @@ _crono
 				SRC=$(w3m -cookie -debug -dump_source -o accept_encoding=='*;q=0' "$URL/chat/titans/changeRoom/?close_clan_msg=true" -o user_agent="$(shuf -n1 .ua)")
 				ACCESS=$(echo $SRC | sed "s/value\=/value\=\n/g" | grep '\<table' | cut -d\" -f2 | head -n1)
 				SND1="Enviar"
-#//
+#/
 echo -e "s=$ACCESS&text="`cat << EOF
 C.C. ok
 EOF`"&send_message=$SND1" >chat.txt
-#\\
+#\
 				SRC=$(w3m -cookie -debug -post chat.txt -dump_source -o accept_encoding=='*;q=0' "$URL/chat/clan/changeRoom/?close_clan_msg=true" -o user_agent="$(shuf -n1 .ua)")
 				_clancoliseum
 				sleep 300
 			fi
 		done
 		_crono
-# //Clan tournament 11:00:00 - 19:00:00
+# /Clan tournament 11:00:00 - 19:00:00
 	elif [[ $HOUR -eq 10 && $MIN -ge 45 || $HOUR -eq 18 && $MIN -ge 45 ]] ; then
 		START=`date +%M`
 		while [[ $MIN -ge 45 && $MIN -le 59 ]] ; do
@@ -73,18 +73,18 @@ EOF`"&send_message=$SND1" >chat.txt
 				SRC=$(w3m -cookie -debug -dump_source -o accept_encoding=='*;q=0' "$URL/chat/titans/changeRoom/?close_clan_msg=true" -o user_agent="$(shuf -n1 .ua)")
 				ACCESS=$(echo $SRC | sed "s/value\=/value\=\n/g" | grep '\<table' | cut -d\" -f2 | head -n1)
 				SND1="Enviar"
-#//
+#/
 echo -e "s=$ACCESS&text="`cat << EOF
 C.T. ok
 EOF`"&send_message=$SND1" >chat.txt
-#\\
+#\
 				SRC=$(w3m -cookie -debug -post chat.txt -dump_source -o accept_encoding=='*;q=0' "$URL/chat/clan/changeRoom/?close_clan_msg=true" -o user_agent="$(shuf -n1 .ua)")
 				_clanfight
 				sleep 300
 			fi
 		done
 		_crono
-# //King of the Immortals 12:30:00 - 16:30:00 - 22:30:00
+# /King of the Immortals 12:30:00 - 16:30:00 - 22:30:00
 	elif [[ $HOUR -eq 12 && $MIN -ge 15 && $MIN -le 30 || $HOUR -eq 16 && $MIN -ge 15 && $MIN -le 30 || $HOUR -eq 22 && $MIN -ge 15 && $MIN -le 30 ]] ; then
 		START=`date +%M`
 		while [[ $MIN -ge 15 && $MIN -le 30 ]] ; do
@@ -98,7 +98,7 @@ EOF`"&send_message=$SND1" >chat.txt
 			fi
 		done
 		_crono
-# //Ancient Altars 14:00:00 - 21:00:00
+# /Ancient Altars 14:00:00 - 21:00:00
 	elif [[ $HOUR -eq 13 && $MIN -ge 45 || $HOUR -eq 20 && $MIN -ge 45 ]] ; then
 		START=`date +%M`
 		while [[ $MIN -ge 45 && $MIN -le 55 ]] ; do
@@ -113,11 +113,11 @@ EOF`"&send_message=$SND1" >chat.txt
 			SRC=$(w3m -cookie -debug -dump_source -o accept_encoding=='*;q=0' "$URL/chat/clan/changeRoom/?close_clan_msg=true" -o user_agent="$(shuf -n1 .ua)")
 			ACCESS=$(echo $SRC | sed "s/value\=/value\=\n/g" | grep '\<table' | cut -d\" -f2 | head -n1)
 			SND1="Enviar"
-#//
+#/
 echo -e "s=$ACCESS&text="`cat << EOF
 A.A. ok
 EOF`"&send_message=$SND1" >chat.txt
-#\\
+#\
 			SRC=$(w3m -cookie -debug -post chat.txt -dump_source -o accept_encoding=='*;q=0' "$URL/chat/clan/changeRoom/?close_clan_msg=true" -o user_agent="$(shuf -n1 .ua)")
 			_altars
 			sleep 300
