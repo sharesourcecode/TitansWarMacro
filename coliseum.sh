91a646c5d8c63a95492fab057fdea87d5aa50e89
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2020-01-27 04:19:54 -0300

    Add files via upload

--- coliseum.sh
+++ coliseum.sh
@@ -69,5 +69,6 @@ _coliseum () {
 	w3m -cookie -debug -o accept_encoding=='*;q=0' $URL/coliseum -o user_agent="$(shuf -n1 .ua)" | head -n15 | sed "/\[user\]/d;/\[arrow\]/d;/\ \[/d" | grep --color "$ACC"
 	echo "Coliseum (✔)"
 	sleep 60
+	SRC=$(w3m -cookie -debug -o accept_encoding=='*;q=0' $URL/settings/graphics/0 -o user_agent="$(shuf -n1 .ua)")
 }
 
