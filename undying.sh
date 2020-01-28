2cf9f30c4cbca0fe22444b1395a1c8bf4147db39
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2020-01-28 07:05:13 -0300

    Update undying.sh

--- undying.sh
+++ undying.sh
@@ -27,7 +27,7 @@ _undying () {
 		echo -e " 🎲 hiting..."
 		SRC=$(w3m -debug -dump_source -o accept_encoding=='*;q=0' "$URL$HIT" -o user_agent="$(shuf -n1 .ua)")
 		_access
-		sleep 2.5
+		sleep 2.2
 	done
 # /view
 	echo ""
