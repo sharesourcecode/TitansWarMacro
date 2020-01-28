ee2dca2f4915801538c4a72af00f8db4be5fbf0c
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2020-01-27 23:23:46 -0300

    Add files via upload

--- coliseum.sh
+++ coliseum.sh
@@ -1,18 +1,22 @@
 _coliseum () {
-# /enterFight
+# //enterFight
 	SRC=$(w3m -cookie -debug -o accept_encoding=='*;q=0' $URL/settings/graphics/1 -o user_agent="$(shuf -n1 .ua)")
-	HPER=41 # //heal on 34% - defaut
-	RPER=12 # //random if enemy have +12% hp - default
-	ITVL=1.6
+	PAGE=coliseum
+	HPER='41' # //heal on 34% - defaut
+	RPER='12' # //random if enemy have +12% hp - default
+	ITVL='1.6'
 	CLSM=( 'coliseum/atk' 'coliseum/atkrnd' 'coliseum/dodge' 'coliseum/heal' 'coliseum/leaveFight' 'coliseum/enterFight' )
-	echo -e "\nColiseum"
+	echo -e "\n$PAGE"
 	echo $URL
 	w3m -cookie -debug -o accept_encoding=='*;q=0' $URL/coliseum/?end_fight=true -o user_agent="$(shuf -n1 .ua)" | head -n11 | tail -n7 | sed "/\[2hit/d;/\[str/d;/combat/d"
+#	lynx -cfg=~/twm/cfg1 -debug $URL/coliseum/?end_fight=true -useragent="$(shuf -n1 .ua)" | head -n11 | tail -n7 | sed "/\[2hit/d;/\[str/d;/combat/d"
 	SRC=$(w3m -cookie -debug -dump_source -o accept_encoding=='*;q=0' $URL/$PAGE -o user_agent="$(shuf -n1 .ua)")
+#	SRC=$(lynx -cfg=~/twm/cfg1 -source $URL/$PAGE -useragent="$(shuf -n1 .ua)")
 	ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep "${CLSM[5]}" | head -n1 | cut -d\' -f2)
 	echo -e " 👣 Entering...\n$ACCESS"
 	SRC=$(w3m -cookie -debug -dump_source -o accept_encoding=='*;q=0' ""$URL$ACCESS"?end_fight=true" -o user_agent="$(shuf -n1 .ua)")
-# /wait
+#	SRC=$(lynx -cfg=~/twm/cfg1 -source ""$URL$ACCESS"?end_fight=true" -useragent="$(shuf -n1 .ua)")
+# //wait
 	echo " 😴 Waiting..."
         EXIT=$(echo $SRC | sed 's/href=/\n/g' | grep "${CLSM[4]}" | head -n1 | cut -d\' -f2)
 	START=`date +%M`
@@ -21,6 +25,7 @@ _coliseum () {
                 [[ $END -gt 4 ]] && break
 		echo -e " 💤	...\n$ACCESS"
 		SRC=$(w3m -cookie -debug -dump_source -o accept_encoding=='*;q=0' $URL/$PAGE -o user_agent="$(shuf -n1 .ua)")
+#		SRC=$(lynx -cfg=~/twm/cfg=1 -source $URL/$PAGE -useragent="$(shuf -n1 .ua)")
 		ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep "$PAGE" | head -n1 | cut -d\' -f2)
 		EXIT=$(echo $SRC | sed 's/href=/\n/g' | grep "${CLSM[4]}" | head -n1 | cut -d\' -f2)
 	done
@@ -29,7 +34,6 @@ _coliseum () {
 	WDRED=$(echo $SRC | sed "s/alt/\\n/g" | grep 'hp' | head -n1 | cut -d\' -f4) #white/dred
 	FULL=$(echo $SRC | sed "s/alt/\\n/g" | grep 'hp' | head -n1 | cut -d\< -f2 | cut -d\> -f2 | tr -cd '[[:digit:]]')
 	HEAL=$(expr $FULL \* $HPER \/ 100)
-	PRTCT=$(echo $SRC | grep -io '<b>ueliton</b>')
 	_show () {
 		HP1=$(echo $SRC | sed "s/alt/\\n/g" | grep 'hp' | head -n1 | cut -d\< -f2 | cut -d\> -f2 | tr -cd '[[:digit:]]')
 		HP2=$(echo $SRC | sed "s/alt/\\n/g" | grep 'hp' | head -n3 | tail -1 | cut -d\< -f1 |cut -d\; -f2 | tr -cd '[[:digit:]]')
@@ -38,7 +42,7 @@ _coliseum () {
 	_show
 	START=`date +%M`
 	while [[ -n $EXIT && -n $ACCESS ]] ; do
-# /function random
+# //function random - - - - - - - - - - - - - - - - - - - - - - - - - - -
                 END=$(expr `date +%M` \- $START)
                 [[ $END -gt 4 ]] && break
 		_random () {
@@ -46,54 +50,53 @@ _coliseum () {
 			SRC=$(w3m -cookie -debug -dump_source -o accept_encoding=='*;q=0' "$URL$ACCESS" -o user_agent="$(shuf -n1 .ua)")
 #			SRC=$(lynx -cfg=~/twm/cfg1 -source -o "$URL$ACCESS" -useragent="$(shuf -n1 .ua)")
 			echo $URL
-			PRTCT=$(echo $SRC | grep -io '<b>ueliton</b>')
 			ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep "${CLSM[1]}" | head -n1 | cut -d\' -f2)
 			_show
 			EXIT=$(echo $SRC | sed 's/href=/\n/g' | grep "${CLSM[0]}" | head -n1 | cut -d\' -f2)
 			WDRED=$(echo $SRC | sed "s/alt/\\n/g" | grep 'hp' | head -n1 | cut -d\' -f4) #white
 			sleep $ITVL
 		}
-# /function dodge
+# //function dodge - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 		_dodge () {
 			echo '🛡️'
 			SRC=$(w3m -cookie -debug -dump_source -o accept_encoding=='*;q=0' "$URL$ACCESS" -o user_agent="$(shuf -n1 .ua)")
 #			SRC=$(lynx -cfg=~/twm/cfg1 -source "$URL$ACCESS" -useragent="$(shuf -n1 .ua)")
 			echo $URL
-			PRTCT=$(echo $SRC | grep -io '<b>ueliton</b>')
 			ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep "${CLSM[2]}" | head -n1 | cut -d\' -f2)
 			_show
 			EXIT=$(echo $SRC | sed 's/href=/\n/g' | grep "${CLSM[0]}" | head -n1 | cut -d\' -f2)
 			WDRED=$(echo $SRC | sed "s/alt/\\n/g" | grep 'hp' | head -n1 | cut -d\' -f4) #white
 			sleep $ITVL
 		}
-# /heal
+# //heal - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 #	    if [[ $WDRED == dred && $HP1 -lt $HEAL ]] ; then
 		if [[ $HP1 -lt $HEAL ]] ; then
 			echo "🆘 HP < $HPER%"
 			SRC=$(w3m -cookie -debug -dump_source -o accept_encoding=='*;q=0' "$URL$ACCESS" -o user_agent="$(shuf -n1 .ua)")
+#			SRC=$(lynx -cfg=~/twm/cfg1 -source "$URL$ACCESS" -useragent="$(shuf -n1 .ua)")
 			echo $URL
-			PRTCT=$(echo $SRC | grep -io '<b>ueliton</b>')
 			ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep "${CLSM[3]}" | head -n1 | cut -d\' -f2)
 			_show
 			EXIT=$(echo $SRC | sed 's/href=/\n/g' | grep "${CLSM[0]}" | head -n1 | cut -d\' -f2)
 			WDRED=$(echo $SRC | sed "s/alt/\\n/g" | grep 'hp' | head -n1 | cut -d\' -f4) #white
 			HP1=$HPFULL
-			ITVL=2.6
+			ITVL='2.5'
 			sleep $ITVL
 			_dodge
 			_random
-# /random
-		elif [[ -n $PRTCT || $WDRED == white && `expr $HP1 + $HP1 \* $RPER \/ 100` -lt $HP2 ]] ; then
+# //random - - - - - - - - - - - - - - - - - - - - - - - - - - -
+		elif [[ $WDRED == white && `expr $HP1 + $HP1 \* $RPER \/ 100` -lt $HP2 ]] ; then
 			_random
 			_dodge
-# /dodge
+# //dodge - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 		else
 			_dodge
 		fi
 	done
-# /view
+# //view - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	echo ""
 	w3m -cookie -debug -o accept_encoding=='*;q=0' $URL/$PAGE -o user_agent="$(shuf -n1 .ua)" | head -n15 | sed "/\[user\]/d;/\[arrow\]/d;/\ \[/d" | grep --color "$ACC"
+#	lynx -cfg=~/twm/cfg1 $URL/$PAGE -useragent="$(shuf -n1 .ua)" | head -n15 | sed "/\[user\]/d;/\[arrow\]/d;/\ \[/d" | grep --color "$ACC"
 	echo "$PAGE (✔)"
 	SRC=$(w3m -cookie -debug -o accept_encoding=='*;q=0' $URL/settings/graphics/0 -o user_agent="$(shuf -n1 .ua)")
 }
