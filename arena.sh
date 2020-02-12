cf0006361169830b20a015d58c48294a09212552
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2020-02-12 10:09:28 -0300

    Add files via upload

--- arena.sh
+++ arena.sh
@@ -1,6 +1,12 @@
 # /arena
 function _arena () {
 	PAC=( 'arena/attack' 'lab/wizard' ) #Page:Action:Condition
+	if [[ -z $CLD ]] ; then
+		SRC=$(w3m -cookie -debug -dump_source -o accept_encoding=='*;q=0' "$URL/clan/$CLD/quest/take/3" -o user_agent="$(shuf -n1 .ua)")
+		SRC=$(w3m -cookie -debug -dump_source -o accept_encoding=='*;q=0' "$URL/clan/$CLD/quest/help/3" -o user_agent="$(shuf -n1 .ua)")
+		SRC=$(w3m -cookie -debug -dump_source -o accept_encoding=='*;q=0' "$URL/clan/$CLD/quest/take/4" -o user_agent="$(shuf -n1 .ua)")
+		SRC=$(w3m -cookie -debug -dump_source -o accept_encoding=='*;q=0' "$URL/clan/$CLD/quest/help/4" -o user_agent="$(shuf -n1 .ua)")
+	fi
 	SRC=$(w3m -cookie -debug -dump_source -o accept_encoding=='*;q=0' "$URL/${PAC[0]:0:5}" -o user_agent="$(shuf -n1 .ua)")
 	ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep "${PAC[0]}" | head -n1 | cut -d\' -f2) #/arena/attack/1/1234567*/
 	EXIT=$(echo $SRC | sed 's/href=/\n/g' | grep "${PAC[1]}" | head -n1 | cut -d\' -f2) #/lab/wizard/potion/1234567*/?ref=/arena/
@@ -10,4 +16,10 @@ function _arena () {
 		echo "$ACCESS"
 		EXIT=$(echo $SRC | sed 's/href=/\n/g' | grep "${PAC[1]}" | head -n1 | cut -d\' -f2) #/lab/wizard/potion/1234567*/?ref=/arena/
 	done
+	if [[ -z $CLD ]] ; then
+		SRC=$(w3m -cookie -debug -dump_source -o accept_encoding=='*;q=0' "$URL/clan/$CLD/quest/end/3" -o user_agent="$(shuf -n1 .ua)")
+		SRC=$(w3m -cookie -debug -dump_source -o accept_encoding=='*;q=0' "$URL/clan/$CLD/quest/deleteHelp/3" -o user_agent="$(shuf -n1 .ua)")
+		SRC=$(w3m -cookie -debug -dump_source -o accept_encoding=='*;q=0' "$URL/clan/$CLD/quest/end/4" -o user_agent="$(shuf -n1 .ua)")
+		SRC=$(w3m -cookie -debug -dump_source -o accept_encoding=='*;q=0' "$URL/clan/$CLD/quest/deleteHelp/4" -o user_agent="$(shuf -n1 .ua)")
+	fi
 }
--- career.sh
+++ career.sh
@@ -2,6 +2,10 @@
 # /career/attack/?r=8781779
 _career () {
 	echo "Checking career..."
+	if [[ -z $CLD ]] ; then
+		SRC=$(w3m -debug -dump_source -o accept_encoding=='*;q=0' "$URL/clan/$CLD/quest/take/6" -o user_agent="$(shuf -n1 .ua)")
+		SRC=$(w3m -debug -dump_source -o accept_encoding=='*;q=0' "$URL/clan/$CLD/quest/help/6" -o user_agent="$(shuf -n1 .ua)")
+	fi
 	SRC=$(w3m -debug -dump_source -o accept_encoding=='*;q=0' "$URL/career" -o user_agent="$(shuf -n1 .ua)")
 	ENTER=$(echo $SRC | sed 's/href=/\n/g' | grep -o 'career/attack')
 	ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep '/career/attack/' | head -n1 | cut -d\' -f2)
@@ -11,5 +15,9 @@ _career () {
 		ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep '/career/attack/' | head -n1 | cut -d\' -f2)
 		echo " ⚔ $ACCESS"
 	done
+	if [[ -z $CLD ]] ; then
+		SRC=$(w3m -debug -dump_source -o accept_encoding=='*;q=0' "$URL/clan/$CLD/quest/deleteHelp/6" -o user_agent="$(shuf -n1 .ua)")
+		SRC=$(w3m -debug -dump_source -o accept_encoding=='*;q=0' "$URL/clan/$CLD/quest/end/6" -o user_agent="$(shuf -n1 .ua)")
+	fi
 	echo -e "career (✔)\n"
 }
