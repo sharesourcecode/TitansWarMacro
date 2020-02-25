cebdab1cd5149c94bfc1e5bae245cf60a97c7a31
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2020-02-25 14:39:41 -0300

    Add files via upload

--- cave.sh
+++ cave.sh
@@ -7,53 +7,28 @@ function _cave () {
 	fi
 	SRC=$(w3m -cookie -dump_source -o accept_encoding=='*;q=0' "$URL/cave/" -o user_agent="$(shuf -n1 .ua)")
 	_condition () {
-		CGTHR=$(echo $SRC | sed 's/href=/\n/g' | grep -o '/cave/gather/')
-		CSPDP=$(echo $SRC | sed 's/href=/\n/g' | grep -o '/cave/speedUp/')
-		CDWN=$(echo $SRC | sed 's/href=/\n/g' | grep '/cave/' | head -n1 | grep -o '/cave/down/')
-		CTTCK=$(echo $SRC | sed 's/href=/\n/g' | grep -o '/cave/attack/')
-	}
-	_action () {
-		AGTHR=$(echo $SRC | sed 's/href=/\n/g' | grep '/cave/gather/' | head -n1 | cut -d\' -f2)
-		ASPDP=$(echo $SRC | sed 's/href=/\n/g' | grep '/cave/speedUp/' | head -n1 | cut -d\' -f2)
-		ADWN=$(echo $SRC | sed 's/href=/\n/g' | grep '/cave/down/' | head -n1 | cut -d\' -f2)
-		ATTCK=$(echo $SRC | sed 's/href=/\n/g' | grep '/cave/attack/' | head -n1 | cut -d\' -f2)
+		ACCESS1=$(echo $SRC | sed 's/href=/\n/g' | grep '/cave/' | head -n1 | cut -d\' -f2)
+		DOWN=$(echo $SRC | sed 's/href=/\n/g' | grep '/cave/down' | cut -d\' -f2)
+		ACCESS2=$(echo $SRC | sed 's/href=/\n/g' | grep '/cave/' | head -n2 | tail -n1 | cut -d\' -f2)
+		ACTION=$(echo $SRC | sed 's/href=/\n/g' | grep '/cave/' | cut -d\' -f2 | tr -cd "[[:alpha:]]")
 	}
 	_condition
-	_action
 	num=0
-	until [[ $num -eq 8 ]]; do
-		if [[ -n CGTHR ]]; then
-			SRC=$(w3m -cookie -dump_source -o accept_encoding=='*;q=0' "$URL$AGTHR" -o user_agent="$(shuf -n1 .ua)")
-			echo $AGTHR
-			_condition
-			_action
-		elif [[ -n CSPDP ]]; then
-			SRC=$(w3m -cookie -dump_source -o accept_encoding=='*;q=0' "$URL$ASPDP" -o user_agent="$(shuf -n1 .ua)")
-			echo $ASPDP
-			_condition
-			_action
-		elif [[ -n CDWN ]]; then
-			SRC=$(w3m -cookie -dump_source -o accept_encoding=='*;q=0' "$URL$ADWN" -o user_agent="$(shuf -n1 .ua)")
-			echo $ADWN
-			_condition
-			_action
-			num=$[$num+1]
-			echo $num
-		elif [[ -n CTTCK ]]; then
-			SRC=$(w3m -cookie -dump_source -o accept_encoding=='*;q=0' "$URL$ATTCK" -o user_agent="$(shuf -n1 .ua)")
-			echo $ATTCK
-			_condition
-			_action
-			SRC=$(w3m -cookie -dump_source -o accept_encoding=='*;q=0' "$URL/cave/runaway" -o user_agent="$(shuf -n1 .ua)")
-			_condition
-			_action
-			SRC=$(w3m -cookie -dump_source -o accept_encoding=='*;q=0' "$URL$ADWN" -o user_agent="$(shuf -n1 .ua)")
-			echo $ADWN
-			_condition
-			_action
-			num=$[$num-1]
-			echo $num
-		fi
+	until [[ $num -eq 9 ]]; do
+		echo $num
+		SRC=$(w3m -cookie -dump_source -o accept_encoding=='*;q=0' "$URL/cave/" -o user_agent="$(shuf -n1 .ua)")
+		_condition
+		case $ACTION in
+			(cavechancercavegatherrcavedownr)
+			SRC=$(w3m -cookie -dump_source -o accept_encoding=='*;q=0' "$URL$ACCESS2" -o user_agent="$(shuf -n1 .ua)") ;;
+			(cavespeedUpr)
+			SRC=$(w3m -cookie -dump_source -o accept_encoding=='*;q=0' "$URL$ACCESS2" -o user_agent="$(shuf -n1 .ua)") ;;
+			(cavedownr|cavedownrclanbuiltprivateUpgradetruerrefcave)
+			num=$[$num+1] ; SRC=$(w3m -cookie -dump_source -o accept_encoding=='*;q=0' "$URL$DOWN" -o user_agent="$(shuf -n1 .ua)") ;;
+			(caveattackrcaverunawayr)
+			num=$[$num+1] ; SRC=$(w3m -cookie -dump_source -o accept_encoding=='*;q=0' "$URL$ACCESS1" -o user_agent="$(shuf -n1 .ua)") ; SRC=$(w3m -cookie -dump_source -o accept_encoding=='*;q=0' "$URL/cave/runaway" -o user_agent="$(shuf -n1 .ua)") ;;
+		esac
+		echo $SRC | sed 's/href=/\n/g' | grep '/cave/' | head -n2 | tail -n1 | cut -d\' -f2
 	done
 	if [[ -n $CLD ]]; then
 		w3m -cookie "$URL/clan/$CLD/quest/end/5" -o user_agent="$(shuf -n1 .ua)" | head -n15 &
