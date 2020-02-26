478a7ae9d05a7d89d7c580af82c4c4e7de54c49e
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2020-02-26 09:25:24 -0300

    Add files via upload

--- cave.sh
+++ cave.sh
@@ -1,6 +1,8 @@
 # /cave
 function _cave () {
 	_clanid
+	w3m -cookie "$URL/clan/$CLD/quest/take/5" -o user_agent="$(shuf -n1 .ua)" | head -n15 &
+	w3m -cookie "$URL/clan/$CLD/quest/help/5" -o user_agent="$(shuf -n1 .ua)" | head -n15 &
 	_condition () {
 		SRC=$(w3m -cookie -dump_source -o accept_encoding=='*;q=0' "$URL/cave/" -o user_agent="$(shuf -n1 .ua)")
 		ACCESS1=$(echo $SRC | sed 's/href=/\n/g' | grep '/cave/' | head -n1 | cut -d\' -f2)
@@ -10,26 +12,22 @@ function _cave () {
 		MEGA=$(echo $SRC | sed 's/src=/\n/g' | grep '/images/icon/silver.png' | grep "'s'" | tail -n1 | grep -o 'M')
 	}
 	_condition
-	if [[ -n $MEGA && -n $CLD ]]; then
-		w3m -cookie "$URL/clan/$CLD/quest/take/5" -o user_agent="$(shuf -n1 .ua)" | head -n15 &
-		w3m -cookie "$URL/clan/$CLD/quest/help/5" -o user_agent="$(shuf -n1 .ua)" | head -n15 &
-	fi
-	_condition
 	num=9
-	until [[ $num -eq 0 || -z $MEGA ]]; do
+	until [[ $num -eq 0 ]]; do
 		_condition
 		case $ACTION in
 			(cavechancercavegatherrcavedownr|cavespeedUpr)
-			SRC=$(w3m -cookie -dump_source -o accept_encoding=='*;q=0' "$URL$ACCESS2" -o user_agent="$(shuf -n1 .ua)") ;;
+				[[ -z $MEGA ]] && num=0
+				SRC=$(w3m -cookie -dump_source -o accept_encoding=='*;q=0' "$URL$ACCESS2" -o user_agent="$(shuf -n1 .ua)") ;;
 			(cavedownr|cavedownrclanbuiltprivateUpgradetruerrefcave)
-			num=$[$num-1] ;
-			SRC=$(w3m -cookie -dump_source -o accept_encoding=='*;q=0' "$URL$DOWN" -o user_agent="$(shuf -n1 .ua)") ;
-			echo $num ;;
+				num=$[$num-1] ;
+				SRC=$(w3m -cookie -dump_source -o accept_encoding=='*;q=0' "$URL$DOWN" -o user_agent="$(shuf -n1 .ua)") ;
+				echo $num ;;
 			(caveattackrcaverunawayr)
-			num=$[$num-1] ;
-			SRC=$(w3m -cookie -dump_source -o accept_encoding=='*;q=0' "$URL$ACCESS1" -o user_agent="$(shuf -n1 .ua)") ;
-			SRC=$(w3m -cookie -dump_source -o accept_encoding=='*;q=0' "$URL/cave/runaway" -o user_agent="$(shuf -n1 .ua)") ;
-			echo $num ;;
+				num=$[$num-1] ;
+				SRC=$(w3m -cookie -dump_source -o accept_encoding=='*;q=0' "$URL$ACCESS1" -o user_agent="$(shuf -n1 .ua)") ;
+				SRC=$(w3m -cookie -dump_source -o accept_encoding=='*;q=0' "$URL/cave/runaway" -o user_agent="$(shuf -n1 .ua)") ;
+				echo $num ;;
 		esac
 		echo $SRC | sed 's/href=/\n/g' | grep '/cave/' | head -n2 | tail -n1 | cut -d\' -f2
 	done
@@ -39,4 +37,3 @@ function _cave () {
 	fi
 	echo -e "cave (✔)\n"
 }
-
