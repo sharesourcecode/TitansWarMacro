6fab2b6e674f9f6e3d5de275cbeb2125c5491a81
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2020-02-29 21:34:54 -0300

    Add files via upload

--- cave.sh
+++ cave.sh
@@ -17,17 +17,22 @@ function _cave () {
 		_condition
 		case $ACTION in
 			(cavechancercavegatherrcavedownr|cavespeedUpr)
-				[[ -z $MEGA ]] && num=0
-				SRC=$(w3m -cookie -dump_source -o accept_encoding=='*;q=0' "$URL$ACCESS2" -o user_agent="$(shuf -n1 .ua)") ;;
+#				[[ -z $MEGA ]] && num=0
+				SRC=$(w3m -cookie -dump_source -o accept_encoding=='*;q=0' "$URL$ACCESS2" -o user_agent="$(shuf -n1 .ua)") ;
+				num=$[$num-1] ;
+				echo $num ;;
 			(cavedownr|cavedownrclanbuiltprivateUpgradetruerrefcave)
 				num=$[$num-1] ;
 				SRC=$(w3m -cookie -dump_source -o accept_encoding=='*;q=0' "$URL$DOWN" -o user_agent="$(shuf -n1 .ua)") ;
 				echo $num ;;
+#				[[ -z $MEGA ]] && num=0 ;;
 			(caveattackrcaverunawayr)
 				num=$[$num-1] ;
 				SRC=$(w3m -cookie -dump_source -o accept_encoding=='*;q=0' "$URL$ACCESS1" -o user_agent="$(shuf -n1 .ua)") ;
 				SRC=$(w3m -cookie -dump_source -o accept_encoding=='*;q=0' "$URL/cave/runaway" -o user_agent="$(shuf -n1 .ua)") ;
 				echo $num ;;
+#				[[ -z $MEGA ]] && num=0 ;;
+			(*) num=0 ;;
 		esac
 		echo $SRC | sed 's/href=/\n/g' | grep '/cave/' | head -n2 | tail -n1 | cut -d\' -f2
 	done
