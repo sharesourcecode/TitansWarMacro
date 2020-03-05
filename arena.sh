bb61976a0676695308caf1ad495e27ef8572b926
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2020-03-05 06:30:39 -0300

    Add files via upload

--- arena.sh
+++ arena.sh
@@ -1,12 +1,16 @@
 # /arena
 function _arena () {
 	_clanid
-#	if [[ -n $CLD ]]; then
-#		w3m -debug -o accept_encoding=='*;q=0' "$URL/clan/$CLD/quest/take/3" -o user_agent="$(shuf -n1 .ua)" | head -n15 &
-#		w3m -debug -o accept_encoding=='*;q=0' "$URL/clan/$CLD/quest/help/3" -o user_agent="$(shuf -n1 .ua)" | head -n15 &
-#		w3m -debug -o accept_encoding=='*;q=0' "$URL/clan/$CLD/quest/take/4" -o user_agent="$(shuf -n1 .ua)" | head -n15 &
-#		w3m -debug -o accept_encoding=='*;q=0' "$URL/clan/$CLD/quest/help/4" -o user_agent="$(shuf -n1 .ua)" | head -n15 &
-#	fi
+#	SRC=$(w3m -debug -dump_source -o accept_encoding=='*;q=0' "$URL/collfight/enterFight" -o user_agent="$(shuf -n1 .ua)")
+#	ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep 'collfight/take' | head -n1 | cut -d\' -f2) #/arena/attack/1/1234567*/
+#	SRC=$(w3m -debug -dump_source -o accept_encoding=='*;q=0' "$URL$ACCESS" -o user_agent="$(shuf -n1 .ua)")
+#	SRC=$(w3m -debug -dump_source -o accept_encoding=='*;q=0' "$URL/collfight/enterFight" -o user_agent="$(shuf -n1 .ua)")
+	if [[ -n $CLD ]]; then
+		w3m -debug -o accept_encoding=='*;q=0' "$URL/clan/$CLD/quest/take/3" -o user_agent="$(shuf -n1 .ua)" | head -n15 &
+		w3m -debug -o accept_encoding=='*;q=0' "$URL/clan/$CLD/quest/help/3" -o user_agent="$(shuf -n1 .ua)" | head -n15 &
+		w3m -debug -o accept_encoding=='*;q=0' "$URL/clan/$CLD/quest/take/4" -o user_agent="$(shuf -n1 .ua)" | head -n15 &
+		w3m -debug -o accept_encoding=='*;q=0' "$URL/clan/$CLD/quest/help/4" -o user_agent="$(shuf -n1 .ua)" | head -n15 &
+	fi
 	SRC=$(w3m -debug -dump_source -o accept_encoding=='*;q=0' "$URL/arena/" -o user_agent="$(shuf -n1 .ua)")
 	ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep 'arena/attack' | head -n1 | cut -d\' -f2) #/arena/attack/1/1234567*/
 	EXIT=$(echo $SRC | sed 's/href=/\n/g' | grep 'lab/wizard' | head -n1 | cut -d\' -f2) #/lab/wizard/potion/1234567*/?ref=/arena/
@@ -16,15 +20,15 @@ function _arena () {
 		echo "$ACCESS"
 		EXIT=$(echo $SRC | sed 's/href=/\n/g' | grep 'lab/wizard' | head -n1 | cut -d\' -f2) #/lab/wizard/potion/1234567*/?ref=/arena/
 	done
-	SRC=$(w3m -debug -dump_source -o accept_encoding=='*;q=0' "$URL/collfight/enterFight" -o user_agent="$(shuf -n1 .ua)")
-	ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep 'collfight/take' | head -n1 | cut -d\' -f2) #/arena/attack/1/1234567*/
-	SRC=$(w3m -debug -dump_source -o accept_encoding=='*;q=0' "$URL$ACCESS" -o user_agent="$(shuf -n1 .ua)")
-	SRC=$(w3m -debug -dump_source -o accept_encoding=='*;q=0' "$URL/collfight/enterFight" -o user_agent="$(shuf -n1 .ua)")
-#	if [[ -n $CLD ]]; then
-#		w3m -debug -o accept_encoding=='*;q=0' "$URL/clan/$CLD/quest/end/3" -o user_agent="$(shuf -n1 .ua)" | head -n15 &
-#		w3m -debug -o accept_encoding=='*;q=0' "$URL/clan/$CLD/quest/deleteHelp/3" -o user_agent="$(shuf -n1 .ua)" | head -n15 &
-#		w3m -debug -o accept_encoding=='*;q=0' "$URL/clan/$CLD/quest/end/4" -o user_agent="$(shuf -n1 .ua)" | head -n15 &
-#		w3m -debug -o accept_encoding=='*;q=0' "$URL/clan/$CLD/quest/deleteHelp/4" -o user_agent="$(shuf -n1 .ua)" | head -n15 &
-#	fi
+#	SRC=$(w3m -debug -dump_source -o accept_encoding=='*;q=0' "$URL/collfight/enterFight" -o user_agent="$(shuf -n1 .ua)")
+#	ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep 'collfight/take' | head -n1 | cut -d\' -f2) #/arena/attack/1/1234567*/
+#	SRC=$(w3m -debug -dump_source -o accept_encoding=='*;q=0' "$URL$ACCESS" -o user_agent="$(shuf -n1 .ua)")
+#	SRC=$(w3m -debug -dump_source -o accept_encoding=='*;q=0' "$URL/collfight/enterFight" -o user_agent="$(shuf -n1 .ua)")
+	if [[ -n $CLD ]]; then
+		w3m -debug -o accept_encoding=='*;q=0' "$URL/clan/$CLD/quest/end/3" -o user_agent="$(shuf -n1 .ua)" | head -n15 &
+		w3m -debug -o accept_encoding=='*;q=0' "$URL/clan/$CLD/quest/deleteHelp/3" -o user_agent="$(shuf -n1 .ua)" | head -n15 &
+		w3m -debug -o accept_encoding=='*;q=0' "$URL/clan/$CLD/quest/end/4" -o user_agent="$(shuf -n1 .ua)" | head -n15 &
+		w3m -debug -o accept_encoding=='*;q=0' "$URL/clan/$CLD/quest/deleteHelp/4" -o user_agent="$(shuf -n1 .ua)" | head -n15 &
+	fi
 	echo -e "arena (✔)\n"
 }
