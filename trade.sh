5460daf7eda8a6abb2c476ec1683b6a0eff81487
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2020-03-15 13:25:33 -0300

    Add files via upload

--- trade.sh
+++ trade.sh
@@ -13,6 +13,9 @@ function _trade () {
 		echo -e "Exchange (✔)\n"
 	if [[ -n $CLD ]] ; then
 		CODE=$(w3m -debug -dump_source -o accept_encoding=='*;q=0' $URL/arena/quit -o user_agent="$(shuf -n1 .ua)" | sed "s/href='/\n/g" | grep "attack/1" | head -n1 | cut -d\/ -f5 | tr -cd "[[:digit:]]")
+		echo -e "/clan/$CLD/money/?r=$CODE&silver=1000&gold=2&confirm=true&type=limit"
+		w3m -debug -dump "$URL/clan/$CLD/money/?r=$CODE&silver=1000&gold=1&confirm=true&type=limit" -o user_agent="$(shuf -n1 .ua)"
+		CODE=$(w3m -debug -dump_source -o accept_encoding=='*;q=0' $URL/arena/quit -o user_agent="$(shuf -n1 .ua)" | sed "s/href='/\n/g" | grep "attack/1" | head -n1 | cut -d\/ -f5 | tr -cd "[[:digit:]]")
 		echo -e "/clan/$CLD/money/?r=$CODE&silver=1000&gold=1&confirm=true&type=limit"
 		w3m -debug -dump "$URL/clan/$CLD/money/?r=$CODE&silver=1000&gold=1&confirm=true&type=limit" -o user_agent="$(shuf -n1 .ua)"
 		echo -e "Clan Money (✔)\n"
