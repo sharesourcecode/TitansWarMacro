e78df7f06dc56e3433945ef5d33855079cf18e42
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2020-08-29 11:37:28 -0300

    Add files via upload

--- coliseum.sh
+++ coliseum.sh
@@ -1,9 +1,10 @@
+
 _coliseum () {
 # /enterFight
 #	SRC=$(w3m -debug $ENC $URL/settings/graphics/1 -o user_agent="$(shuf -n1 .ua)")
 	HPER='27'
 	RPER='5'
-	ITVL='2.9' #2.8mini.
+	ITVL='2.7987' #1.8, 1.9
 	echo -e "\nColiseum"
 	echo $URL
 	w3m -debug $ENC $URL/coliseum/ -o user_agent="$(shuf -n1 .ua)" | head -n11 | tail -n7 | sed "/\[2hit/d;/\[str/d;/combat/d"
