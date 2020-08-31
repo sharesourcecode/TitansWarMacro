fb96c7267729309214cf92176067cb10c5c9ff14
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2020-08-30 22:58:19 -0300

    Update flagfight.sh

--- flagfight.sh
+++ flagfight.sh
@@ -3,7 +3,7 @@ _flagfight () {
 #	SRC=$(w3m -debug $ENC $URL/settings/graphics/1 -o user_agent="$(shuf -n1 .ua)")
 	HPER='49'
 	RPER='9'
-	ITVL='2.9'
+	ITVL='2.92'
 	echo -e "\nFlag Fight"
 	echo $URL
 	SRC=$(w3m -debug -dump_source $ENC $URL/flagfight/?close=reward -o user_agent="$(shuf -n1 .ua)")
