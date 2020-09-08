ffb8781481b32364fd10365945a13ad43bed8f30
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2020-09-08 00:35:21 -0300

    Add files via upload

--- proxy.sh
+++ proxy.sh
@@ -2,8 +2,8 @@ _proxy () {
 #Downlod proxy list
 	unset http_proxy && \
 	>bestproxy.txt; >testproxy.txt && \
-	w3m -o accept_encoding=='*;q=0' -dump_source "https://m.freevpn4you.net/free-proxy.php" -o user_agent="Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1" | grep -oP '(\d+\.\d+\.\d+\.\d+)' | sed 's/^.*$/&:80/' >bestproxy.txt && \
-}
+	w3m -o accept_encoding=='*;q=0' -dump_source "https://m.freevpn4you.net/free-proxy.php" -o user_agent="Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1" | grep -oP '(\d+\.\d+\.\d+\.\d+)' | sed 's/^.*$/&:80/' >bestproxy.txt
+
 #test proxy list
 #	echo -e "\nLooking for a better proxy...\n"
 #	for proxy in `cat testproxy.txt`
@@ -16,4 +16,5 @@ _proxy () {
 #	[[ -z $proxy ]] && cat testproxy.txt | head -n1 >bestproxy.txt
 #	echo -e "Current proxy $proxy"
 #	w3m meuip.com
-#}
+#
+}
