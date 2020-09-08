fe3238c7731b9f9d832d335594da917991805899
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2020-09-08 07:09:48 -0300

    Add files via upload

--- requeriments.sh
+++ requeriments.sh
@@ -7,52 +7,52 @@ _requeriments () {
 	echo -e "👉 Please wait...☕👴"
 	_sync () {
 		curl https://github.com/sharesourcecode/twm/raw/master/cave.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 1/20"
+		echo -e "\n Upgrading 1/19"
 		curl https://github.com/sharesourcecode/twm/raw/master/PLAY.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 2/20"
+		echo -e "\n Upgrading 2/19"
 		curl https://github.com/sharesourcecode/twm/raw/master/altars.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 3/20"
+		echo -e "\n Upgrading 3/19"
 		curl https://github.com/sharesourcecode/twm/raw/master/arena.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 4/20"
+		echo -e "\n Upgrading 4/19"
 		curl https://github.com/sharesourcecode/twm/raw/master/campaign.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 5/20"
+		echo -e "\n Upgrading 5/19"
 		curl https://github.com/sharesourcecode/twm/raw/master/career.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 6/20"
+		echo -e "\n Upgrading 6/19"
 		curl https://github.com/sharesourcecode/twm/raw/master/clancoliseum.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 7/20"
+		echo -e "\n Upgrading 7/19"
 		curl https://github.com/sharesourcecode/twm/raw/master/clandungeon.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 8/20"
+		echo -e "\n Upgrading 8/19"
 		curl https://github.com/sharesourcecode/twm/raw/master/clanfight.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 9/20"
+		echo -e "\n Upgrading 9/19"
 		curl https://github.com/sharesourcecode/twm/raw/master/clanid.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 10/20"
+		echo -e "\n Upgrading 10/19"
 		curl https://github.com/sharesourcecode/twm/raw/master/coliseum.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 11/20"
+		echo -e "\n Upgrading 11/19"
 		curl https://github.com/sharesourcecode/twm/raw/master/crono.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 12/20"
+		echo -e "\n Upgrading 12/19"
 		curl https://github.com/sharesourcecode/twm/raw/master/king.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 13/20"
+		echo -e "\n Upgrading 13/19"
 		curl https://github.com/sharesourcecode/twm/raw/master/loginlogoff.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 14/20"
+		echo -e "\n Upgrading 14/19"
 		curl https://github.com/sharesourcecode/twm/raw/master/play.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 15/20"
+		echo -e "\n Upgrading 15/19"
 		curl https://github.com/sharesourcecode/twm/raw/master/requeriments.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 16/20"
+		echo -e "\n Upgrading 16/19"
 		curl https://github.com/sharesourcecode/twm/raw/master/trade.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 17/20"
+		echo -e "\n Upgrading 17/19"
 		curl https://github.com/sharesourcecode/twm/raw/master/undying.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 18/20"
+		echo -e "\n Upgrading 18/19"
 		curl https://github.com/sharesourcecode/twm/raw/master/flagfight.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 19/20"
+		echo -e "\n Upgrading 19/19"
 		curl https://github.com/sharesourcecode/twm/raw/master/proxy.sh -O -L &> /dev/null ;
-		echo -e "\n Upgrading 20/20"
+		echo -e "\n Upgrading 20/19"
 		dos2unix *.sh &> /dev/null
 	}
 # /termux on android
 	termux-wake-lock &> /dev/null
 	if [[ $? = 0 ]] ; then
 		[[ ! -e executed.txt ]] && pkg install termux-api w3m curl -y && >executed.txt
-#		[[ $(date +%H) -lt 10 || $(date +%H) -gt 22 ]] && _sync
+		[[ $(date +%H) -lt 10 || $(date +%H) -gt 22 ]] && _sync
 		reset; clear
 		echo -e "Successful updates!\n"
 	else
@@ -63,8 +63,6 @@ _requeriments () {
 	cd $HOME/twm
 # /user agents to $HOME/.tmp/.ua
 	echo -e '"Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:52.0) Gecko/20100101 Firefox/52.0"' >.ua
-#	echo -e '"Mozilla/5.0 (Linux; Android 7.1.2; Moto G) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.81 Mobile Safari/537.36"' >.ua
-#	echo -e '"Mozilla/5.0 (Android 7.1.2; Mobile; rv:80.0) Gecko/80.0 Firefox/80.0"' >.ua #firefox mobile
 	dos2unix ~/twm/.ua &> /dev/null
 	cp ~/twm/.ua ~/.tmp/.ua &> /dev/null
 # /servers
