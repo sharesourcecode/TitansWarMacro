e194aa6fa522485bbf6c296a41a9e0ff4d2931f5
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2020-09-11 22:09:26 -0300

    Update clanid.sh

--- clanid.sh
+++ clanid.sh
@@ -1,5 +1,6 @@
 # /clan id
 _clanid () {
 	SRC=$(w3m -debug -dump_source $ENC "$URL/clan" -o user_agent="$(shuf -n1 .ua)")
-	CLD=$(echo $SRC | sed "s/\/clan\//\\n/g" | grep 'built\/' | cut -d\/ -f1)
+	CLD=$(echo $SRC | sed "s/\/clan\//\\n/g" | grep 'built\/' | awk -F\/ '{ print $1 }')
+	unset SRC
 }
