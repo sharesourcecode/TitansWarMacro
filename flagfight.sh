700f2ceec0102a463d1d45f570bd20f94079dd22
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2020-09-11 22:14:23 -0300

    Update flagfight.sh

--- flagfight.sh
+++ flagfight.sh
@@ -1,13 +1,12 @@
 _flagfight () {
 # /enterFight
-#	SRC=$(w3m -debug $ENC $URL/settings/graphics/1 -o user_agent="$(shuf -n1 .ua)")
 	HPER='49'
 	RPER='9'
-	ITVL='2.92'
+	ITVL='3.5'
 	echo -e "\nFlag Fight"
 	echo $URL
 	SRC=$(w3m -debug -dump_source $ENC $URL/flagfight/?close=reward -o user_agent="$(shuf -n1 .ua)")
-	ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep 'flagfight/enterFight' | head -n1 | cut -d\' -f2)
+	ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep 'flagfight/enterFight' | head -n1 | awk -F\' '{ print $2 }')
 	echo -e " 👣 Entering...\n$ACCESS"
 # /wait
 	echo " 😴 Waiting..."
@@ -16,12 +15,15 @@ _flagfight () {
 		[[ $(date +%M) = 15 && $(date +%S) > 19 ]] && break
 		echo -e " 💤	...\n$ACCESS"
 		SRC=$(w3m -debug -dump_source $ENC "$URL$ACCESS" -o user_agent="$(shuf -n1 .ua)")
-		ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep '/flagfight/' | head -n1 | cut -d\' -f2)
+		ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep '/flagfight/' | head -n1 | awk -F\' '{ print $2 }')
 		EXIT=$(echo $SRC | sed 's/href=/\n/g' | grep -o 'flagfight/attack/')
 	done
-	FULL=$(echo $SRC | sed "s/alt/\\n/g" | grep 'hp' | head -n1 | cut -d\< -f2 | cut -d\> -f2 | tr -cd '[[:digit:]]')
+	FULL=$(echo $SRC | sed "s/alt/\\n/g" | grep 'hp' | head -n1 | awk -F\< '{ print $2 }' | awk -F\> '{ print $2 }' | tr -cd '[[:digit:]]')
 	_access
 	HP3=$HP1
+	ddg=4
+	hl=18
+	grss=12
 	until [[ -n $BEXIT && -z $OUTGATE ]] ; do
 		[[ $(date +%M) = 2[67] ]] && break
 # /dodge
@@ -88,11 +90,12 @@ _flagfight () {
 			grss=$[$grss+1]
 		fi
 	done
+	unset HPER RPER ITVL SRC ACCESS EXIT FULL HP3 ddg hl grss
 # /view
 	echo ""
-	w3m -debug $ENC $URL/flagfight -o user_agent="$(shuf -n1 .ua)" | head -n15 | tail -n14 | sed "/\[user\]/d;/\[arrow\]/d;/\ \[/d"
+	w3m -debug $ENC $URL/flagfight -o user_agent="$(shuf -n1 .ua)" | head -n15 | tail -n14 | sed "/\[user\]/d;/\[arrow\]/d;/\ \[/d" | grep --color $ACC
+	_unset
 	echo "Flag Fight (✔)"
-#	SRC=$(w3m -debug $ENC $URL/settings/graphics/0 -o user_agent="$(shuf -n1 .ua)")
 #	_AtakeHelp
 	_fullmana
 #	_AdeleteEnd
