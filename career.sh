afbe96671708845f374edbf7856cc7e6d4d64783
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2020-09-11 22:04:45 -0300

    Update career.sh

--- career.sh
+++ career.sh
@@ -10,17 +10,17 @@ _career () {
 #	fi
 	SRC=$(w3m -debug -dump_source $ENC "$URL/career" -o user_agent="$(shuf -n1 .ua)")
 	ENTER=$(echo $SRC | sed 's/href=/\n/g' | grep -o 'career/attack')
-	ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep '/career/attack/' | head -n1 | cut -d\' -f2)
+	ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep '/career/attack/' | head -n1 | awk -F\' '{ print $2 }')
 	until [[ -z $ENTER ]]; do
 		SRC=$(w3m -debug -dump_source $ENC "$URL$ACCESS" -o user_agent="$(shuf -n1 .ua)")
 		ENTER=$(echo $SRC | sed 's/href=/\n/g' | grep -o 'career/attack')
-		ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep '/career/attack/' | head -n1 | cut -d\' -f2)
+		ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep '/career/attack/' | head -n1 | awk -F\' '{ print $2 }')
 		echo " ⚔ $ACCESS"
 	done
 #	if [[ -n $CLD ]]; then
 #		w3m -debug $ENC "$URL/clan/$CLD/quest/deleteHelp/6" -o user_agent="$(shuf -n1 .ua)" | head -n15
 #		w3m -debug $ENC "$URL/clan/$CLD/quest/end/6" -o user_agent="$(shuf -n1 .ua)" | head -n15
 #	fi
+	unset CLD SRC ENTER ACCESS
 	echo -e "career (✔)\n"
 }
-
