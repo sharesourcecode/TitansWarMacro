8a7b40f886b12015346d1228a856c3af0778e12f
Ton Guitar <49400447+sharesourcecode@users.noreply.github.com>
2020-09-11 22:06:22 -0300

    Update clandungeon.sh

--- clandungeon.sh
+++ clandungeon.sh
@@ -4,14 +4,15 @@ function _clandungeon () {
 	if [[ -n $CLD ]]; then
 		echo "Checking Clan Dungeon..."
 		SRC=$(w3m -debug -dump_source $ENC "$URL/clandungeon/?close" -o user_agent="$(shuf -n1 .ua)")
-		ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep '/clandungeon/attack/' | head -n1 | cut -d\' -f2)
+		ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep '/clandungeon/attack/' | head -n1 | awk -F\' '{ print $2 }')
 		EXIST=$(echo $SRC | sed 's/href=/\n/g' | grep -o 'clandungeon/attack')
 		until [[ -z $EXIST ]]; do
 			SRC=$(w3m -debug -dump_source $ENC "$URL$ACCESS" -o user_agent="$(shuf -n1 .ua)")
-			ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep '/clandungeon/attack/' | head -n1 | cut -d\' -f2)
+			ACCESS=$(echo $SRC | sed 's/href=/\n/g' | grep '/clandungeon/attack/' | head -n1 | awk -F\' '{ print $2 }')
 			EXIST=$(echo $SRC | sed 's/href=/\n/g' | grep -o 'clandungeon/attack')
 			echo " ⚔ $ACCESS"
 		done
 		echo -e "Clan Dungeon (✔)\n"
 	fi
+	unset CLD SRC ACCESS EXIST
 }
